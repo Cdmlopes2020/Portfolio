2592e62b258dc317bfc8f733289f141c481bcbd8
Carlos Lopes <48572688+Cdmlopes2020@users.noreply.github.com>
2020-03-07 23:11:22 +0000

    Add files via upload

---
+++ portefolio.php
@@ -0,0 +1,101 @@
+<?php 
+
+require('nav.php') ;
+?> 
+
+  <section class="ftco-section bg-light">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-md-8 col-lg-10 order-md-last">
+    				<div class="row">
+		    			<div class="col-sm-6 col-md-6 col-lg-4 ftco-animate">
+		    				<div class="product">
+		    					<a href="product1.php" class="img-prod"><img class="img-fluid" src="images/TESTERENDER3.png" alt="Colorlib Template">
+		    						<span class="status">50%</span>
+		    						<div class="overlay"></div>
+		    					</a>
+		    					<div class="text py-3 px-3">
+		    						<h3><a href="#">Cenário 3d </a></h3>
+		    						<div class="d-flex">
+		    							<div class="pricing">
+				    						<p class="price"><span class="mr-2 price-dc">$100.00</span><span class="price-sale">$50.00</span></p>
+				    					</div>
+				    					<div class="rating">
+			    							<p class="text-right">
+			    								<a href="#"><span class="ion-ios-star-outline"></span></a>
+			    								<a href="#"><span class="ion-ios-star-outline"></span></a>
+			    								<a href="#"><span class="ion-ios-star-outline"></span></a>
+			    								<a href="#"><span class="ion-ios-star-outline"></span></a>
+			    								<a href="#"><span class="ion-ios-star-outline"></span></a>
+			    							</p>
+			    						</div>
+			    					</div>
+			    					<p class="bottom-area d-flex px-3">
+		    							<a href="#" class="add-to-cart text-center py-2 mr-1"><span>Add to cart <i class="ion-ios-add ml-1"></i></span></a>
+		    							<a href="#" class="buy-now text-center py-2">Buy now<span><i class="ion-ios-cart ml-1"></i></span></a>
+		    						</p>
+		    					</div>
+		    				</div>
+		    			</div>
+		    		
+		    			
+		    	
+		    	
+		    		</div>
+		    		<div class="row mt-5">
+		          <div class="col text-center">
+		            <div class="block-27">
+		              <ul>
+		                <li><a href="#">&lt;</a></li>
+		                <li class="active"><span>1</span></li>
+		                <li><a href="#">2</a></li>
+		                <li><a href="#">3</a></li>
+		                <li><a href="#">4</a></li>
+		                <li><a href="#">5</a></li>
+		                <li><a href="#">&gt;</a></li>
+		              </ul>
+		            </div>
+		          </div>
+		        </div>
+		    	</div>
+
+		    	<div class="col-md-4 col-lg-2 sidebar">
+		    		<div class="sidebar-box-2">
+		    			<h2 class="heading mb-4"><a href="#">Digital</a></h2>
+		    			<ul>
+		    				<li><a href="#">Design Gráfico</a></li>
+								<li><a href="#">Tipografia</a></li>
+								<li><a href="#">Modelação 3d</a></li>
+								<li><a href="#">Vídeo</a></li>
+								<li><a href="#">Imagem</a></li>
+								<li><a href="#">Áudio</a></li>
+								<li><a href="#">Fotografia</a></li>
+								<li><a href="#">Desenvolvimento Web</a></li>
+								<li><a href="#">Apps &amp; Jogos </a></li>
+		    			</ul>
+		    		</div>
+		    		<div class="sidebar-box-2">
+		    			<h2 class="heading mb-4"><a href="#">Tradicional</a></h2>
+		    			<ul>
+		    				<li><a href="#">Shirts &amp; Tops</a></li>
+								<li><a href="#">Pintura</a></li>
+								<li><a href="#">Escultura</a></li>
+								<li><a href="#">Gravura</a></li>
+								<li><a href="#">Serigrafia</a></li>
+								<li><a href="#">Desenho</a></li>
+		    			</ul>
+		    		</div>
+		    		
+    			</div>
+    		</div>
+    	</div>
+    </section>
+
+
+
+
+
+
+<?php 
+require('footer.php') ;
+?> 
---
+++ product1.php
@@ -0,0 +1,46 @@
+<?php
+require('nav.php');
+?>
+
+
+<style>
+    .space{ padding-top: 50px;}
+    .button.btn.center{text-align: center; !important
+    }
+
+</style>
+ 
+
+<div>
+
+    <div class="row space">
+        
+         <div class="col-2">  </div>
+        <div class="col-4"> 
+        
+        <img src="images/TESTERENDER3.png" width="100%"> 
+        
+        </div>
+        
+        
+         <div class="col-4"> 
+        
+        <img src="images/TESTERENDER3.png" width="100%"> 
+        
+        </div>
+        
+        <div class="col-2">  </div>
+        
+
+</div>
+    
+      <div class="row space">
+          <div class="col-4">   </div>
+    <div class="col-4"> 
+  <a href="portefolio.php"> <button type="button" class="btn btn-dark" ><b> OTHER WORKS </b></button></a>
+</div>
+    
+     </div>
+    
+
+ </div>
